e41616f263d671c8de2b7a3d4df524e38255211e
James Krikelas <james@localhost.localdomain>
2021-02-09 12:30:22 -0600

    portfolio page generator

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
 /*
 const printProfileData = profileDataArr => {
     // This...
@@ -14,4 +22,8 @@ const printProfileData = profileDataArr => {
 
 printProfileData(profileDataArgs);
 */
-const geratePage = () => 'Name: Jane, Github: janehub';
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+    console.log('Portfolio complete! Check out index.html to see the output!');
+})
